0f6b9fae4091157977962287fd78f5b118d23781
Kanishk Banker <kanishksanjaybanker@gmail.com>
2026-04-24 11:30:00 +0530

    chore: comment out server listen function for future use

--- server.ts
+++ server.ts
@@ -26,8 +26,8 @@ mongoose.connect(`${process.env.MONGO_URI}task-manager`)
   .then(() => console.log('Connected to MongoDB!'))
   .catch((err) => console.log('Database connection failed:', err));
 
-app.listen(4000, () => {
-  console.log("Server is running!");
-});
+// app.listen(4000, () => {
+//   console.log("Server is running!");
+// });
 
 export default app;
